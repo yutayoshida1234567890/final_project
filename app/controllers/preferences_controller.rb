897d020c2db04f0c4174fa67cb7bef254fe6a3a3
Raghu Betina <raghu@firstdraft.com>
2019-06-06 21:22:34 +0000

    Only preference user can access show page

--- app/controllers/preferences_controller.rb
+++ app/controllers/preferences_controller.rb
@@ -1,5 +1,5 @@
 class PreferencesController < ApplicationController
-  before_action :current_user_must_be_preference_user, :only => [:edit_form, :update_row, :destroy_row]
+  before_action :current_user_must_be_preference_user, :only => [:show, :edit_form, :update_row, :destroy_row]
 
   def current_user_must_be_preference_user
     preference = Preference.find(params["id_to_display"] || params["prefill_with_id"] || params["id_to_modify"] || params["id_to_remove"])

230af4d5bb7601ad6ee99d3e53466c7391c38b6c
Raghu Betina <raghu@firstdraft.com>
2019-06-06 21:22:16 +0000

    Whitelist columns in Participant for ActiveAdmin

--- app/admin/participants.rb
+++ app/admin/participants.rb
@@ -1,4 +1,6 @@
 ActiveAdmin.register Participant do
+
+ permit_params :event_id, :user_id, :location
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #

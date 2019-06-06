fe65cc00a422d3df02fdc32fcddee31db7b91d85
Raghu Betina <raghu@firstdraft.com>
2019-06-06 21:22:34 +0000

    Only preference user can modify preference

--- app/controllers/preferences_controller.rb
+++ app/controllers/preferences_controller.rb
@@ -1,4 +1,14 @@
 class PreferencesController < ApplicationController
+  before_action :current_user_must_be_preference_user, :only => [:edit_form, :update_row, :destroy_row]
+
+  def current_user_must_be_preference_user
+    preference = Preference.find(params["id_to_display"] || params["prefill_with_id"] || params["id_to_modify"] || params["id_to_remove"])
+
+    unless current_user == preference.user
+      redirect_to :back, :alert => "You are not authorized for that."
+    end
+  end
+
   def index
     @preferences = Preference.all
 

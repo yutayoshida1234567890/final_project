c47b01ebe010d92f5555ff17dcecde3be610e60c
Raghu Betina <raghu@firstdraft.com>
2019-06-06 21:21:33 +0000

    Direct association between restaurants and ratings

--- app/models/rating.rb
+++ app/models/rating.rb
@@ -1,6 +1,9 @@
 class Rating < ApplicationRecord
   # Direct associations
 
+  belongs_to :restaurant,
+             :counter_cache => true
+
   belongs_to :user,
              :counter_cache => true
 
--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,6 +1,9 @@
 class Restaurant < ApplicationRecord
   # Direct associations
 
+  has_many   :ratings,
+             :dependent => :destroy
+
   # Indirect associations
 
   # Validations
